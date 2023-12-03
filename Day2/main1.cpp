4a2e8dd2dd765253ecf25f4c881e3a43d2980d46
Deyan Dyankov <118269569+ddyankov28@users.noreply.github.com>
2023-12-03 20:13:43 +0000

    aoc

--- Day2/main1.cpp
+++ Day2/main1.cpp
@@ -1,8 +1,23 @@
 # include <iostream>
 # include <fstream>
+# include <string>
+
 # define MAX_RED 12
 # define MAX_GREEN 13
 # define MAX_BLUE 14
+
+void    removeSpaces(std::string& line)
+{
+    std::string::iterator it = line.begin();
+    while (it != line.end())
+    {
+        if (isspace(*it))
+            line.erase(it);
+        else
+            ++it;
+    }
+}
+
 
 int main(int ac, char **av)
 {
@@ -12,6 +27,8 @@ int main(int ac, char **av)
     while (getline(file, line))
     {
         int gameID = 1;
+        removeSpaces(line);
+        std::string newLine = substr(line.find(':', 0));
         std::cout << line << std::endl;
     }
 
